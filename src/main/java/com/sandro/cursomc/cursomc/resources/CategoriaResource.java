b3cc47f90139dccb20bf80e2475db6a72f2be864
Alex Oliveira <alexnosaint@email.com>
2019-01-27 17:02:22 -0300

    Testando o REST

---
+++ src/main/java/com/sandro/cursomc/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.sandro.cursomc.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@GetMapping()
+	private String listar() {
+		return "REST OK!!";
+	}
+}
